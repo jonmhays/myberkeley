eecca0093833c75199f1b8cbb496617c30f5de51
Ray Davis <ray@media.berkeley.edu>
2012-02-14 11:16:33 -0800

    MYB-1394 Convert any rogue HTML entities into UTF-8 before storing attributes

--- provision/src/main/java/edu/berkeley/myberkeley/provision/CalOaeAuthorizableService.java
+++ provision/src/main/java/edu/berkeley/myberkeley/provision/CalOaeAuthorizableService.java
@@ -26,6 +26,7 @@ import edu.berkeley.myberkeley.api.provision.OaeAuthorizableService;
 import edu.berkeley.myberkeley.api.provision.ProvisionResult;
 import edu.berkeley.myberkeley.api.provision.SynchronizationState;
 import org.apache.commons.lang.RandomStringUtils;
+import org.apache.commons.lang.StringEscapeUtils;
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.Service;
@@ -193,7 +194,13 @@ public class CalOaeAuthorizableService implements OaeAuthorizableService {
         JSONObject elementsJson = new JSONObject();
         for (String key : profileProps) {
           JSONObject itemJson = new JSONObject();
-          itemJson.put("value", attributes.get(key));
+          
+          // Some clever people have managed to inject HTML entities (rather than UTF-8
+          // strings) into campus data sources. For functional compatibility with
+          // bSpace and CalNet directory displays, try to decode them.
+          final String value = StringEscapeUtils.unescapeHtml((String) attributes.get(key));
+          
+          itemJson.put("value", value);
           elementsJson.put(key, itemJson);
         }
         JSONObject sectionJson = new JSONObject();
